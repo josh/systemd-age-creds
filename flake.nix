bccc7e0e5da8718c4171d1639dcba5b72fae1c96
Joshua Peek <josh@users.noreply.github.com>
2024-10-21 14:05:45 -0700

    Add basic nixos module

--- flake.nix
+++ flake.nix
@@ -1,5 +1,5 @@
 {
-  description = "A very basic flake";
+  description = "systemd-age-creds";
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/24.05";
@@ -19,5 +19,51 @@
         aarch64-linux.default = self.packages.aarch64-linux.systemd-age-creds;
         x86_64-linux.default = self.packages.x86_64-linux.systemd-age-creds;
       };
+
+      nixosModules.default = (
+        {
+          config,
+          system,
+          pkgs,
+          lib,
+        }:
+        let
+          cfg = config.systemd-age-creds;
+        in
+        {
+          options.systemd-age-creds = {
+            enable = lib.mkEnableOption "Enable age credentials service";
+
+            package = lib.mkOption {
+              type = lib.types.package;
+              default = self.packages.${system}.default;
+              defaultText = lib.literalExpression "pkgs.systemd-age-creds";
+              description = "The package to use for systemd-age-creds.";
+            };
+          };
+
+          config = lib.mkIf cfg.enable {
+            systemd.sockets.age-creds = {
+              description = "age credentials socket";
+              wantedBy = [ "sockets.target" ];
+
+              socketConfig = {
+                ListenStream = "/run/age-creds.socket";
+                SocketMode = "0600";
+                Service = "age-creds.service";
+              };
+            };
+
+            systemd.services.age-creds = {
+              description = "age credentials service";
+              requires = [ "age-creds.socket" ];
+              serviceConfig = {
+                Type = "simple";
+                ExecStart = "${lib.getExe cfg.package}";
+              };
+            };
+          };
+        }
+      );
     };
 }
