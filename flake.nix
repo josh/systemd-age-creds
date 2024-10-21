aa489646d21c58b040f6695fdcd8bd5aefc11ec4
Joshua Peek <josh@users.noreply.github.com>
2024-10-21 19:49:21 +0000

    Stub system outputs

--- flake.nix
+++ flake.nix
@@ -2,14 +2,11 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
   };
 
   outputs = { self, nixpkgs }: {
-
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
-
+    packages.aarch64-linux.default = self.packages.aarch64-linux.hello;
     packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
-
   };
 }
