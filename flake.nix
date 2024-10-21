714790e05bfc38518574dfb2e238894e37881e9f
Joshua Peek <josh@users.noreply.github.com>
2024-10-21 13:31:00 -0700

    Switch to stable branch

--- flake.nix
+++ flake.nix
@@ -2,11 +2,13 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/24.05";
   };
 
-  outputs = { self, nixpkgs }: {
-    packages.aarch64-linux.default = nixpkgs.legacyPackages.aarch64-linux.hello;
-    packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.hello;
-  };
+  outputs =
+    { self, nixpkgs }:
+    {
+      packages.aarch64-linux.default = nixpkgs.legacyPackages.aarch64-linux.hello;
+      packages.x86_64-linux.default = nixpkgs.legacyPackages.x86_64-linux.hello;
+    };
 }
