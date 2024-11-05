1dc8f1622f9ec7f9de6bf547bf334cce0a2a6620
Joshua Peek <josh@users.noreply.github.com>
2024-11-05 12:15:48 -0800

    Copy creds to tmp

--- nix/tests/nixos-system-unit.nix
+++ nix/tests/nixos-system-unit.nix
@@ -1,9 +1,11 @@
 {
   lib,
   self,
-  age,
   runCommandLocal,
   testers,
+  writeShellScript,
+  age,
+  coreutils,
   testName,
   creds,
   socketAccept ? false,
@@ -31,6 +33,12 @@ let
       ''
         age-keygen -o $out
       '';
+
+  copyCredsScript = writeShellScript "export-creds.bash" ''
+    ${coreutils}/bin/mkdir /tmp/age-creds-test
+    ${coreutils}/bin/cp $CREDENTIALS_DIRECTORY/* /tmp/age-creds-test/
+  '';
+
 in
 
 assert credCount > 0;
@@ -41,19 +49,17 @@ testers.runNixOSTest {
   node.pkgsReadOnly = false;
 
   nodes.machine =
-    {
-      config,
-      pkgs,
-      ...
-    }:
+    { config, ... }:
     {
       imports = [ self.nixosModules.default ];
+
       services.systemd-age-creds = {
         enable = true;
         identity = identityFile;
         directory = credstoreDir;
         inherit socketAccept;
       };
+
       systemd.services.age-creds-test = {
         wantedBy = [ "multi-user.target" ];
         serviceConfig = {
@@ -62,7 +68,7 @@ testers.runNixOSTest {
           LoadCredential = builtins.map (
             name: "${name}:${config.services.systemd-age-creds.socket}"
           ) credNames;
-          ExecStart = "${pkgs.bash}/bin/bash -c '${pkgs.coreutils}/bin/cp -r %d/* /root/'";
+          ExecStart = copyCredsScript;
         };
       };
     };
@@ -81,13 +87,13 @@ testers.runNixOSTest {
       assert n_connections == 0, f"Expected 0 active connection, got {n_connections}"
       assert n_accepted == len(creds), f"Expected {len(creds)} accepted connections, got {n_accepted}"
 
-    files = machine.succeed("ls /root").splitlines()
-    assert len(files) > 0, "Expected at least one file in /root"
+    files = machine.succeed("ls /tmp/age-creds-test").splitlines()
+    assert len(files) > 0, "Expected at least one file in /tmp/age-creds-test"
     assert len(files) == len(creds), f"Expected {len(creds)} files, got {len(files)}: {files}"
 
     for name, value in creds.items():
       assert name in files, f"Expected file {name}"
-      contents = machine.succeed(f"cat /root/{name}").strip()
+      contents = machine.succeed(f"cat /tmp/age-creds-test/{name}").strip()
       assert contents == value, f"Expected {name} to equal '{value}', got '{contents}'"
   '';
 }
