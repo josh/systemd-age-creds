c9fdc1dc9519fbf4427e17355fc07da1ead01619
Joshua Peek <josh@users.noreply.github.com>
2024-12-24 19:56:17 -0800

    Pass CGO_ENABLED as env when supported
    Closes #22

--- nix/systemd-age-creds.nix
+++ nix/systemd-age-creds.nix
@@ -5,35 +5,43 @@
 }:
 let
   version = "0.0.0";
+
+  # https://github.com/NixOS/nixpkgs/pull/359641
+  enableCGO =
+    if builtins.hasAttr "CGO_ENABLED" (lib.functionArgs buildGoModule) then
+      { CGO_ENABLED = 0; }
+    else
+      { env.CGO_ENABLED = 0; };
 in
-buildGoModule {
-  pname = "systemd-age-creds";
-  inherit version;
-  src = lib.sources.sourceByRegex ./.. [
-    ".*\.go$"
-    "^go.mod$"
-    "^go.sum$"
-    "^test$"
-    "^test\/.*$"
-  ];
-  vendorHash = null;
+buildGoModule (
+  {
+    pname = "systemd-age-creds";
+    inherit version;
+    src = lib.sources.sourceByRegex ./.. [
+      ".*\.go$"
+      "^go.mod$"
+      "^go.sum$"
+      "^test$"
+      "^test\/.*$"
+    ];
+    vendorHash = null;
 
-  CGO_ENABLED = 0;
+    ldflags = [
+      "-s"
+      "-w"
+      "-X main.Version=${version}"
+      "-X main.AgeBin=${lib.getExe age}"
+    ];
 
-  ldflags = [
-    "-s"
-    "-w"
-    "-X main.Version=${version}"
-    "-X main.AgeBin=${lib.getExe age}"
-  ];
+    nativeBuildInputs = [ age ];
 
-  nativeBuildInputs = [ age ];
-
-  meta = {
-    description = "Load age encrypted credentials in systemd units";
-    mainProgram = "systemd-age-creds";
-    homepage = "https://github.com/josh/systemd-age-creds";
-    license = lib.licenses.mit;
-    platforms = lib.platforms.linux;
-  };
-}
+    meta = {
+      description = "Load age encrypted credentials in systemd units";
+      mainProgram = "systemd-age-creds";
+      homepage = "https://github.com/josh/systemd-age-creds";
+      license = lib.licenses.mit;
+      platforms = lib.platforms.linux;
+    };
+  }
+  // enableCGO
+)
